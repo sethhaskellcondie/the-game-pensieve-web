09b938562b7100bf3d1dd04f9372ae8ad6d24b18
Seth Condie <sethhaskellcondie@gmail.com>
2025-08-15 16:20:51 -0600

    Created the initial page, with a header, footer, and sidebard.

--- src/app/components/container/container.component.ts
+++ src/app/components/container/container.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from "../button/button.component";
+import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from "../footer/footer.component";
+import { SidebarComponent } from "../sidebar/sidebar.component";
 
 @Component({
   selector: 'app-container',
   standalone: true,
-  imports: [CommonModule, ButtonComponent],
+  imports: [CommonModule, ButtonComponent, HeaderComponent, FooterComponent, SidebarComponent],
   templateUrl: './container.component.html',
   styleUrl: './container.component.scss'
 })
